5dbe6665f17afb188501d9438c0678091cb3b50d
Stipe Stanic <stipe.stanic156@gmail.com>
2021-10-27 18:15:10 +0200

    Updated form page

--- react_tools/src/components/FormContainer.js
+++ react_tools/src/components/FormContainer.js
@@ -1,22 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { FaPhone, FaEnvelope } from "react-icons/fa";
 
 const Form = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form className="form">
+    <form className="form" onSubmit={handleSubmit}>
       <h2>CONTACT US</h2>
       <p type="Name:">
-        <input placeholder="Write your name here.."></input>
+        <input
+          id="name"
+          name="name"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          placeholder="Write your name here.."
+        ></input>
       </p>
       <p type="Email:">
-        <input placeholder="Let us know how to contact you back.."></input>
+        <input
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Let us know how to contact you back.."
+        ></input>
       </p>
       <p type="Message:">
-        <input placeholder="What would you like to tell us.."></input>
+        <input
+          id="message"
+          name="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          type="text"
+          placeholder="What would you like to tell us.."
+        ></input>
       </p>
-      <button>Send Message</button>
+      <button type="submit">Send Message</button>
       <div>
-        <span className="fa fa-phone"></span>001 1023 567
-        <span className="fa fa-envelope-o"></span> contact@company.com
+        <span className="fa fa-phone">
+          <FaPhone />
+        </span>
+        001 1023 567
+        <span className="fa fa-envelope-o">
+          <FaEnvelope />
+        </span>{" "}
+        contact@company.com
       </div>
     </form>
   );
